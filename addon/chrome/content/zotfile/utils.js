a4f3bc2819315172381b15c7df496cdcb3dfee4d
MuiseDestiny <51939531+MuiseDestiny@users.noreply.github.com>
2023-08-01 15:26:06 +0800

    new version

--- addon/chrome/content/zotfile/utils.js
+++ addon/chrome/content/zotfile/utils.js
@@ -64,8 +64,8 @@ Zotero.ZotFile.Utils = new (function () {
 
       return OS.Path.normalize(
         getCollectionPath(collection.parentID) +
-        Zotero.ZotFile.folderSep +
-        collection.name,
+          Zotero.ZotFile.folderSep +
+          collection.name,
       );
     };
 
@@ -179,7 +179,7 @@ Zotero.ZotFile.Utils = new (function () {
       try {
         wrk.open(tryKeys[i].root, tryKeys[i].path, wrk.ACCESS_READ);
         progId = wrk.readStringValue(tryKeys[i].value);
-      } catch (e) { }
+      } catch (e) {}
     }
 
     if (!progId) {
@@ -191,7 +191,7 @@ Zotero.ZotFile.Utils = new (function () {
     try {
       wrk.open(wrk.ROOT_KEY_CLASSES_ROOT, progId + "\\CurVer", wrk.ACCESS_READ);
       progId = wrk.readStringValue("") || progId;
-    } catch (e) { }
+    } catch (e) {}
 
     //get command
     var success = false;
@@ -203,7 +203,7 @@ Zotero.ZotFile.Utils = new (function () {
       try {
         wrk.open(wrk.ROOT_KEY_CLASSES_ROOT, tryKeys[i], wrk.ACCESS_READ);
         success = true;
-      } catch (e) { }
+      } catch (e) {}
     }
 
     if (!success) {
